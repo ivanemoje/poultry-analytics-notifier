3783bd04ddb0bbbb131827eae9cfc94815bd740d
Ivan EMOJE <29137316+ivanemoje@users.noreply.github.com>
2025-06-25 16:51:27 +0200

    update

--- scripts/egg_report.sh
+++ scripts/egg_report.sh
@@ -141,7 +141,7 @@ cat <<EOF
 
 🧺 Trays: \`$latest_trays\`
 🥚 Eggs: \`$latest_eggs\`
-❌ Broken eggs: \`$latest_eggs_broken\`
+🥚 Broken: \`$latest_eggs_broken\`
 
 🥚 Total Eggs (this entry): \`$((latest_trays * 30 + latest_eggs))\`
 
