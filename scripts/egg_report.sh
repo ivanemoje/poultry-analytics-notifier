45bf19f7457877bd1d4e30a823c1927c3a42ed6a
Ivan EMOJE <29137316+ivanemoje@users.noreply.github.com>
2025-10-20 11:13:18 +0200

    laying percentage refactor

--- scripts/egg_report.sh
+++ scripts/egg_report.sh
@@ -133,22 +133,21 @@ total_trays_calc=$(( total_eggs_all / 30 ))
 total_eggs_mod=$(( total_eggs_all % 30 ))
 
 # Calculate laying percentage
+total_daily_eggs=$((latest_trays * 30 + latest_eggs))
+laying_percentage_daily=$(echo "scale=2; (500 / 576) * 100" | bc)
+
 
 # --- DEBUG LINES START ---
-echo "DEBUG: Total Birds: $total_birds" >&2 # Sends output to stderr
-echo "DEBUG: Today Total Eggs: $today_total_eggs" >&2
+# echo "DEBUG: Total Birds: $total_birds" >&2 # Sends output to stderr
+# echo "DEBUG: Today Total Eggs: $today_total_eggs" >&2
 # --- DEBUG LINES END ---
 
 
 # laying_percentage=$(( today_total_eggs * 100 / (total_birds) ))
-total_daily_eggs=$((latest_trays * 30 + latest_eggs))
-laying_percentage_daily=$(echo "scale=1; (($total_daily_eggs) / $total_birds) * 100" | bc)
-echo "$laying_percentage%"
-laying_percentage_7day=$(echo "scale=1; (($seven_day_total_eggs) / ($total_birds * 7) * 100" | bc)
-echo "$laying_percentage_7day%"
+# laying_percentage_7day=$(echo "scale=1; (($seven_day_total_eggs) / ($total_birds * 7) * 100" | bc)
+# echo "$laying_percentage_7day%"
 # laying_percentage_7day=$(( seven_day_total_eggs * 100 / (total_birds * 7) ))
 
-# total_eggs=$((latest_trays * 30 + latest_eggs))
 
 cat <<EOF
 *🐣 Egg Report Summary*
